a4be5c5a1104ef87208a90ad0e5f5868fc054bc9
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-08-24 15:08:03 +1000

    feat: finish limbs

--- src/svgs/BeanieMan.vue
+++ src/svgs/BeanieMan.vue
@@ -1,4 +1,9 @@
 <script>
+const getLineEnd = (x1, y1, lineXPercent, lineYUp, lineLength) => {
+    const x2 = x1 + lineXPercent * lineLength;
+    const y2 = y1 + Math.sqrt(1 - lineXPercent * lineXPercent) * lineLength * (lineYUp ? -1 : 1);
+    return [x2, y2]
+}
 export default {
     props: {
         minimumX: {
@@ -25,8 +30,58 @@ export default {
         const headWidth = this.lengthX / 10 * 8;
         const headCentreX = headX + headWidth / 2;
         const beanieFoldHeight = this.lengthY / 15;
-        const beanieFoldY =  headY;
+        const beanieFoldY = headY;
         const beanieStripeHeight = beanieFoldHeight / 5;
+        const leftArmPosition = {
+            elbowXPercent: -0.3,
+            elbowYUp: false,
+            wristXPercent: -0.2,
+            wristYUp: false,
+            fingertipXPercent: 0.4,
+            fingertipYUp: true,
+        }
+        const rightArmPosition = {
+            elbowXPercent: 0.5,
+            elbowYUp: false,
+            wristXPercent: 0.2,
+            wristYUp: false,
+            fingertipXPercent: 0.4,
+            fingertipYUp: false,
+        }
+        const leftLegPosition = {
+            kneeXPercent: -0.3,
+            kneeYUp: false,
+            ankleXPercent: -0.2,
+            ankleYUp: false,
+        }
+        const rightLegPosition = {
+            kneeXPercent: 0.2,
+            kneeYUp: false,
+            ankleXPercent: 0.2,
+            ankleYUp: false,
+        }
+        const bodyLength = this.lengthY / 10 * 3;
+        const bodyTopY = headY + headHeight;
+        const bodyY2 = bodyTopY + bodyLength;
+        const upperArmLength = bodyLength * 0.51;
+        const lowerArmLength = bodyLength * 0.51;
+        const upperLegLength = bodyLength * 0.58;
+        const lowerLegLength = bodyLength * 0.58;
+        const handLength = bodyLength * 0.2;
+        const upperArmX1 = headCentreX;
+        const upperArmY1 = bodyTopY + 0.2 * bodyLength;
+        const upperLegX1 = headCentreX;
+        const [rightUpperArmX2, rightUpperArmY2] = getLineEnd(upperArmX1, upperArmY1, rightArmPosition.elbowXPercent, rightArmPosition.elbowYUp, upperArmLength);
+        const [leftUpperArmX2, leftUpperArmY2] = getLineEnd(upperArmX1, upperArmY1, leftArmPosition.elbowXPercent, leftArmPosition.elbowYUp, upperArmLength);
+        const [rightLowerArmX2, rightLowerArmY2] = getLineEnd(rightUpperArmX2, rightUpperArmY2, rightArmPosition.wristXPercent, rightArmPosition.wristYUp, lowerArmLength);
+        const [leftLowerArmX2, leftLowerArmY2] = getLineEnd(leftUpperArmX2, leftUpperArmY2, leftArmPosition.wristXPercent, leftArmPosition.wristYUp, lowerArmLength);
+        const [rightHandX2, rightHandY2] = getLineEnd(rightLowerArmX2, rightLowerArmY2, rightArmPosition.fingertipXPercent, rightArmPosition.fingertipYUp, handLength);
+        const [leftHandX2, leftHandY2] = getLineEnd(leftLowerArmX2, leftLowerArmY2, leftArmPosition.fingertipXPercent, leftArmPosition.fingertipYUp, handLength);
+        const [rightUpperLegX2, rightUpperLegY2] = getLineEnd(headCentreX, bodyY2, rightLegPosition.kneeXPercent, rightLegPosition.kneeYUp, upperLegLength);
+        const [leftUpperLegX2, leftUpperLegY2] = getLineEnd(headCentreX, bodyY2, leftLegPosition.kneeXPercent, leftLegPosition.kneeYUp, upperLegLength);
+        const [rightLowerLegX2, rightLowerLegY2] = getLineEnd(rightUpperLegX2, rightUpperLegY2, rightLegPosition.ankleXPercent, rightLegPosition.ankleYUp, lowerLegLength);
+        const [leftLowerLegX2, leftLowerLegY2] = getLineEnd(leftUpperLegX2, leftUpperLegY2, leftLegPosition.ankleXPercent, leftLegPosition.ankleYUp, lowerLegLength);
+
         return {
             headX,
             headY,
@@ -46,8 +101,31 @@ export default {
             beanieEllipseRadiusY: beanieFoldHeight * 13 / 20,
             bodyX1: headCentreX,
             bodyX2: headCentreX,
-            bodyY1: headY + headHeight,
-            bodyY2: headY + headHeight + this.lengthY / 5 * 2,
+            bodyY1: bodyTopY,
+            bodyY2,
+            upperArmX1,
+            upperArmY1,
+            rightUpperArmX2,
+            rightUpperArmY2,
+            leftUpperArmX2,
+            leftUpperArmY2,
+            rightLowerArmX2,
+            rightLowerArmY2,
+            leftLowerArmX2,
+            leftLowerArmY2,
+            rightHandX2,
+            rightHandY2,
+            leftHandX2,
+            leftHandY2,
+            rightUpperLegX2,
+            rightUpperLegY2,
+            leftUpperLegX2,
+            leftUpperLegY2,
+            rightLowerLegX2,
+            rightLowerLegY2,
+            leftLowerLegX2,
+            leftLowerLegY2,
+
             // beanieSquareY: 20,
             // beanieSquareWidth: 16,
 
@@ -57,16 +135,40 @@ export default {
 </script>
 
 <template>
-    <rect :x="headX" :y="headY" stroke="black" :height="headHeight" :width="headWidth" rx="15" stroke-width="2"
-        fill="transparent">
+    <rect class="stick-head" :x="headX" :y="headY" stroke="black" :height="headHeight" :width="headWidth" rx="15"
+        stroke-width="2" fill="transparent">
     </rect>
-    <rect :x="beanieFoldX" :y="beanieFoldY" :height="beanieFoldHeight" :width="beanieFoldWidth" rx="8" fill="yellow"></rect>
-    <ellipse :cx="beanieCentreX" :cy="beanieEllipseCentreY" :rx="beanieEllipseRadiusX" :ry="beanieEllipseRadiusY" fill="yellow"></ellipse>
-    <rect :x="beanieFoldX" :y="beanieStripe1Y" :height="beanieStripeHeight" :width="beanieFoldWidth" rx="2" fill="black"></rect>
-    <rect :x="beanieFoldX" :y="beanieStripe2Y" :height="beanieStripeHeight" :width="beanieFoldWidth" rx="2" fill="black"></rect>
+    <rect class="beanie-fold" :x="beanieFoldX" :y="beanieFoldY" :height="beanieFoldHeight" :width="beanieFoldWidth"
+        rx="8" fill="yellow"></rect>
+    <ellipse class="beanie-ellipse" :cx="beanieCentreX" :cy="beanieEllipseCentreY" :rx="beanieEllipseRadiusX"
+        :ry="beanieEllipseRadiusY" fill="yellow"></ellipse>
+    <rect class="beanie-stripe" :x="beanieFoldX" :y="beanieStripe1Y" :height="beanieStripeHeight"
+        :width="beanieFoldWidth" rx="2" fill="black"></rect>
+    <rect class="beanie=stripe" :x="beanieFoldX" :y="beanieStripe2Y" :height="beanieStripeHeight"
+        :width="beanieFoldWidth" rx="2" fill="black"></rect>
     <!-- <rect :x="beanieSquareX" :y="beanieSquareY" :width="beanieSquareWidth" :height="beanieSquareWidth"
         :transform="`rotate(45 ${beanieSquareX + beanieSquareWidth / 2} ${beanieSquareY + beanieSquareWidth / 2})`"
         fill="black">
     </rect> -->
-    <line :x1="bodyX1" :y1="bodyY1" :x2="bodyX2" :y2="bodyY2" stroke-width="2" stroke="black"></line>
+    <line class="stick-torso" :x1="bodyX1" :y1="bodyY1" :x2="bodyX2" :y2="bodyY2" stroke-width="2" stroke="black"></line>
+    <line class="stick-upper-arm-right" :x1="upperArmX1" :y1="upperArmY1" :x2="rightUpperArmX2" :y2="rightUpperArmY2"
+        stroke-width="2" stroke="black"></line>
+    <line class="stick-upper-arm-left" :x1="upperArmX1" :y1="upperArmY1" :x2="leftUpperArmX2" :y2="leftUpperArmY2"
+        stroke-width="2" stroke="black"></line>
+    <line class="stick-lower-arm-right" :x1="rightUpperArmX2" :y1="rightUpperArmY2" :x2="rightLowerArmX2"
+        :y2="rightLowerArmY2" stroke-width="2" stroke="black"></line>
+    <line class="stick-lower-arm-left" :x1="leftUpperArmX2" :y1="leftUpperArmY2" :x2="leftLowerArmX2"
+        :y2="leftLowerArmY2" stroke-width="2" stroke="black"></line>
+    <line class="stick-hand-right" :x1="rightLowerArmX2" :y1="rightLowerArmY2" :x2="rightHandX2" :y2="rightHandY2"
+        stroke-width="2" stroke="black"></line>
+    <line class="stick-hand-left" :x1="leftLowerArmX2" :y1="leftLowerArmY2" :x2="leftHandX2" :y2="leftHandY2"
+        stroke-width="2" stroke="black"></line>
+    <line class="stick-upper-leg-right" :x1="bodyX1" :y1="bodyY2" :x2="rightUpperLegX2" :y2="rightUpperLegY2"
+        stroke-width="2" stroke="black"></line>
+    <line class="stick-upper-leg-left" :x1="bodyX1" :y1="bodyY2" :x2="leftUpperLegX2" :y2="leftUpperLegY2"
+        stroke-width="2" stroke="black"></line>
+    <line class="stick-lower-leg-right" :x1="rightUpperLegX2" :y1="rightUpperLegY2" :x2="rightLowerLegX2"
+        :y2="rightLowerLegY2" stroke-width="2" stroke="black"></line>
+    <line class="stick-lower-leg-left" :x1="leftUpperLegX2" :y1="leftUpperLegY2" :x2="leftLowerLegX2"
+        :y2="leftLowerLegY2" stroke-width="2" stroke="black"></line>
 </template>
