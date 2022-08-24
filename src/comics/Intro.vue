d569feb3d202b7a5bec32f9edc15e0d331ae7698
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2022-08-24 11:00:25 +1000

    feat: set up replicable, positionable beanie man

--- src/comics/Intro.vue
+++ src/comics/Intro.vue
@@ -1,54 +1,45 @@
 <script setup>
 import AngryAvatar from '../svgs/AngryAvatar.vue';
+import BeanieMan from '../svgs/BeanieMan.vue';
 import Panel from '../components/Panel.vue';
 import SpeechBubble from '../svgs/SpeechBubble.vue';
 </script>
 
 <template>
 
-    <Panel>
-      <g transform="scale(0.5 0.5) 
-                translate(250 290)">
-        <AngryAvatar />
-      </g>
-      <g transform="scale(-0.5 0.5) 
+  <Panel>
+    <BeanieMan :minimumX="50" :minimumY="50" :lengthX="400" :lengthY="200" />
+    <g transform="translate(100 100)">
+      <text textLength="700">Welcome to my comic!!!</text>
+    </g>
+
+  </Panel>
+  <Panel>
+    <BeanieMan  />
+
+    <g transform="scale(-0.5 0.5) 
                 translate(-450 220)
                 ">
-        <SpeechBubble />
-      </g>
-      <g transform="translate(100 100)">
-        <text textLength="700">Welcome to my comic!!!</text>
-      </g>
+      <SpeechBubble />
+    </g>
+    <g transform="translate(100 100)">
+      <text textLength="700">Welcome to my comic!!!</text>
+    </g>
 
-    </Panel>
-    <Panel>
-      <g transform="scale(0.5 0.5) 
+  </Panel>
+  <Panel>
+    <g transform="scale(0.5 0.5) 
                 translate(250 290)">
-        <AngryAvatar />
-      </g>
-      <g transform="scale(-0.5 0.5) 
+      <AngryAvatar />
+    </g>
+    <g transform="scale(-0.5 0.5) 
                 translate(-450 220)
                 ">
-        <SpeechBubble />
-      </g>
-      <g transform="translate(100 100)">
-        <text textLength="700">Welcome to my comic!!!</text>
-      </g>
+      <SpeechBubble />
+    </g>
+    <g transform="translate(100 100)">
+      <text textLength="700">Welcome to my comic!!!</text>
+    </g>
 
-    </Panel>
-    <Panel>
-      <g transform="scale(0.5 0.5) 
-                translate(250 290)">
-        <AngryAvatar />
-      </g>
-      <g transform="scale(-0.5 0.5) 
-                translate(-450 220)
-                ">
-        <SpeechBubble />
-      </g>
-      <g transform="translate(100 100)">
-        <text textLength="700">Welcome to my comic!!!</text>
-      </g>
-
-    </Panel>
+  </Panel>
 </template>
---
+++ src/svgs/BeanieMan.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+    props: {
+        minimumX: {
+            type: Number,
+            default: 35,
+        },
+        minimumY: {
+            type: Number,
+            default: 35,
+        },
+        lengthX: {
+            type: Number,
+            default: 100,
+        },
+        lengthY: {
+            type: Number,
+            default: 400,
+        },
+    },
+    data() {
+        const headX = this.minimumX + this.lengthX / 10;
+        const headY = this.minimumY + this.lengthY / 5;
+        const headHeight = this.lengthY / 5;
+        const headWidth = this.lengthX / 10 * 8;
+        const headCentreX = headX + headWidth / 2;
+        const beanieFoldHeight = this.lengthY / 15;
+        const beanieFoldY =  headY;
+        const beanieStripeHeight = beanieFoldHeight / 5;
+        return {
+            headX,
+            headY,
+            headWidth,
+            headHeight,
+            beanieSquareX: headX + headWidth / 2,
+            beanieFoldX: this.minimumX + this.lengthX / 15,
+            beanieFoldY,
+            beanieStripe1Y: beanieFoldY + beanieStripeHeight,
+            beanieStripe2Y: beanieFoldY + beanieStripeHeight * 3,
+            beanieFoldHeight,
+            beanieStripeHeight,
+            beanieFoldWidth: this.lengthX / 15 * 13,
+            beanieEllipseRadiusX: this.lengthX / 15 * 11 / 2,
+            beanieCentreX: headCentreX,
+            beanieEllipseCentreY: beanieFoldY,
+            beanieEllipseRadiusY: beanieFoldHeight * 13 / 20,
+            bodyX1: headCentreX,
+            bodyX2: headCentreX,
+            bodyY1: headY + headHeight,
+            bodyY2: headY + headHeight + this.lengthY / 5 * 2,
+            // beanieSquareY: 20,
+            // beanieSquareWidth: 16,
+
+        }
+    }
+}
+</script>
+
+<template>
+    <rect :x="headX" :y="headY" stroke="black" :height="headHeight" :width="headWidth" rx="15" stroke-width="2"
+        fill="transparent">
+    </rect>
+    <rect :x="beanieFoldX" :y="beanieFoldY" :height="beanieFoldHeight" :width="beanieFoldWidth" rx="8" fill="yellow"></rect>
+    <ellipse :cx="beanieCentreX" :cy="beanieEllipseCentreY" :rx="beanieEllipseRadiusX" :ry="beanieEllipseRadiusY" fill="yellow"></ellipse>
+    <rect :x="beanieFoldX" :y="beanieStripe1Y" :height="beanieStripeHeight" :width="beanieFoldWidth" rx="2" fill="black"></rect>
+    <rect :x="beanieFoldX" :y="beanieStripe2Y" :height="beanieStripeHeight" :width="beanieFoldWidth" rx="2" fill="black"></rect>
+    <!-- <rect :x="beanieSquareX" :y="beanieSquareY" :width="beanieSquareWidth" :height="beanieSquareWidth"
+        :transform="`rotate(45 ${beanieSquareX + beanieSquareWidth / 2} ${beanieSquareY + beanieSquareWidth / 2})`"
+        fill="black">
+    </rect> -->
+    <line :x1="bodyX1" :y1="bodyY1" :x2="bodyX2" :y2="bodyY2" stroke-width="2" stroke="black"></line>
+</template>
