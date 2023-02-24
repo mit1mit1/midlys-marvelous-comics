db8442651b09358a2659959f5e890b60877555b1
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2023-02-25 10:58:30 +1100

    tweak: deployment

--- src/comics/AbortionShouldBeMostlyIllegal.vue
+++ src/comics/AbortionShouldBeMostlyIllegal.vue
@@ -1,16 +1,16 @@
-<script setup>
-import BeanieMan from '../svgs/BeanieMan.vue';
-import Panel from '../components/Panel.vue';
-import ComicBox from '../components/ComicBox.vue';
-import AfterComicBox from '../components/AfterComicBox.vue';
-import ComicTitle from '../components/ComicTitle.vue';
-import NavigationBar from '../components/NavigationBar.vue';
-import { availableLimbPositions } from '../limbPositions';
+<script setup lang="ts">
+import BeanieMan from '../vue-comic-components/svgs/BeanieMan.vue';
+import Panel from '../vue-comic-components/components/PanelBox.vue';
+import ComicBox from '../vue-comic-components/components/ComicBox.vue';
+import AfterComicBox from '../vue-comic-components/components/AfterComicBox.vue';
+import ComicTitle from '../vue-comic-components/components/ComicTitle.vue';
+import NavigationBar from '../vue-comic-components/components/NavigationBar.vue';
+import { availableLimbPositions } from '../vue-comic-components/svgs/constants/limbPositions';
 </script>
 
 <template>
   <ComicTitle>Abortions Should Be Mostly Illegal</ComicTitle>
-  <NavigationBar />
+  <NavigationBar :comicList="[]" />
   <ComicBox>
     <Panel>
       <BeanieMan :minimumX="60" :minimumY="110" :lengthX="90" :lengthY="360"
@@ -19,7 +19,7 @@ import { availableLimbPositions } from '../limbPositions';
       <text x="170" y="140">LIFE AND IT'S VALUE.</text>
     </Panel>
   </ComicBox>
-  <NavigationBar />
+  <NavigationBar :comicList="[]" />
   <AfterComicBox>
     <p>Join the discussion at <a
         href="https://www.reddit.com/r/mitt_comics/comments/wxv4ah/comic_2_sydney_evangelicals_what/">Reddit</a>.</p>
--- src/comics/AbortionShouldBeMostlyLegal.vue
+++ src/comics/AbortionShouldBeMostlyLegal.vue
@@ -1,16 +1,16 @@
-<script setup>
-import BeanieMan from '../svgs/BeanieMan.vue';
-import Panel from '../components/Panel.vue';
-import ComicBox from '../components/ComicBox.vue';
-import AfterComicBox from '../components/AfterComicBox.vue';
-import ComicTitle from '../components/ComicTitle.vue';
-import NavigationBar from '../components/NavigationBar.vue';
-import { availableLimbPositions } from '../limbPositions';
+<script setup lang="ts">
+import BeanieMan from '../vue-comic-components/svgs/BeanieMan.vue';
+import Panel from '../vue-comic-components/components/PanelBox.vue';
+import ComicBox from '../vue-comic-components/components/ComicBox.vue';
+import AfterComicBox from '../vue-comic-components/components/AfterComicBox.vue';
+import ComicTitle from '../vue-comic-components/components/ComicTitle.vue';
+import NavigationBar from '../vue-comic-components/components/NavigationBar.vue';
+import { availableLimbPositions } from '../vue-comic-components/svgs/constants/limbPositions';
 </script>
 
 <template>
   <ComicTitle>Abortion Should Be Mostly Legal</ComicTitle>
-  <NavigationBar />
+  <NavigationBar :comicList="[]" />
   <ComicBox>
     <Panel>
       <BeanieMan :minimumX="60" :minimumY="110" :lengthX="90" :lengthY="360"
@@ -21,7 +21,7 @@ import { availableLimbPositions } from '../limbPositions';
       <text x="170" y="220">ILLEGALIZING MAKES.</text>
     </Panel>
   </ComicBox>
-  <NavigationBar />
+  <NavigationBar :comicList="[]" />
   <AfterComicBox>
     <p>Join the discussion at <a
         href="https://www.reddit.com/r/mitt_comics/comments/wxv4ah/comic_2_sydney_evangelicals_what/">Reddit</a>.</p>
